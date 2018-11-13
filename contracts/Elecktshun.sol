4be28008773728507ed6fa5e81424fb133da77e5
Marlon Hendred <mhendred@Marlons-MacBook-Pro.local>
2018-11-13 10:02:10 -0500

    connected front end, displaying candidates and votes

--- contracts/Elecktshun.sol
+++ contracts/Elecktshun.sol
@@ -1,11 +1,25 @@
 pragma solidity ^0.4.2;
 
 contract Elecktshun {
+    // Model candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
     // Store candidate
-    // Read candidate
-    string public candidate;
-    // Constructor
+    // Fetch candidate
+    mapping(uint => Candidate) public candidates;
+    // Store candidate count
+    uint public candidateCount;
+
     function Elecktshun() public {
-        candidate = "Candidate 1";
+        addCandidate("Mardoggy");
+        addCandidate("Heebs");
+    }
+
+    function addCandidate (string _name) private {
+        candidateCount++;
+        candidates[candidateCount] = Candidate(candidateCount, _name, 0);
     }
 }
